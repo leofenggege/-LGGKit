551b0e09e0fd4e8fb1b65c4ef014538c32d59595
Leo <leofenggege@gmail.com>
2021-03-05 11:31:51 +0800

    测试1

--- Source/Core/LGGUIAdapt.m
+++ Source/Core/LGGUIAdapt.m
@@ -154,8 +154,6 @@ BOOL isShapedScreen()
     }
 }
 
-
-
 #pragma mark ============================== 颜色 ====================================
 UIColor *UIColorFromRGB(NSInteger rgbValue) {
     return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0];
